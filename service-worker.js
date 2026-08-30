36d08e1df7ec1f91a50e0bbd2db572628139927f
giostacchio <31801153+giostacchio@users.noreply.github.com>
2026-08-30 08:44:34 +0200

    Bump PWA cache for V5.2 draw-matching fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'pwa-SUPERENALOTTO_ANALYZER_V5_1-v1';
+const CACHE = 'pwa-SUPERENALOTTO_ANALYZER_V5_2-v1';
 const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./apple-touch-icon.png','./favicon.ico'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
